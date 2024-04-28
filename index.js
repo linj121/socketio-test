7edbafa75957946265fec5b4da9d0c28e12be86a
linj121 <jiexulin99@gmail.com>
2024-04-28 05:13:11 -0400

    feat: Add support for nicknames(nickname validation, in-memory nickname storage, display nickname for each broadcasted message); Append message directly after current user sends it, instead of appending the echoed message from socket 'chat message'

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -5,16 +6,40 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const db = [];
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("new user", (user) => {
+    if (user === "") {
+      console.error("User name is empty");
+      io.emit("new user", { error: "name empty", name: user });
+      return;
+    }
+
+    if (db.includes(user)) {
+      console.error(`User ${user} already exists!`);
+      io.emit("new user", { error: "user taken", name: user });
+      return;
+    }
+    db.push(user);
+    console.log(`Current users: ${db}`);
+    io.emit("new user", { error: "", name: user });
+  });
+  socket.on("chat message", (data) => {
+    const { name, content } = data;
+    if (content.startsWith("/reset")) {
+      console.log("DB reset");
+      db.length = 0;
+    } else {
+      io.emit("chat message", data);
+    }
   });
   socket.on("disconnect", () => {
-    io.emit("chat message", "A user has left the chat");
+    io.emit("disconnect message", "Someone has left the chat");
   });
 });
 
